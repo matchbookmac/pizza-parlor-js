3726e6721ccc89c40a7e8f9f16fa98c7e2d8efff
Ian <ian@iancmacdonald.com>
2015-06-05 10:52:52 -0700

    Add Toppings and Order objects.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,45 @@
 
 
 //jQuery
-$( document ).ready(function() {
+$( document ).ready(function () {
     console.log( "jQuery Ready" );
 
     $("#jqtest").text('jQuery Ready')
 });
 
 //raw js
-function Toppings() {
-
+// Addition of Size function to Object
+Object.prototype.size = function (object) {
+  var properties = Object.keys(object);
+  return properties.length;
 };
 
-function Pizza(crust, sauce, toppings) {
-  this.crust    = crust;
-  this.sauce    = sauce;
-  this.toppings = toppings;
+// Toppings
+function Toppings() {
+  for (var i = 0; i < arguments.length; i++) {
+    this[i] = arguments[i];
+  };
+};
+
+Toppings.prototype.thing = function () {
+  return 1;
+};
+
+// Pizza
+function Pizza(size, crust, sauce, toppings) {
+  this.crustSize = size;
+  this.crust     = crust;
+  this.sauce     = sauce;
+  this.toppings  = toppings;
+};
+
+// Order
+function Order() {
+  this.pizzas = [];
+  this.numPizzas = 0;
+};
+
+Order.prototype.save = function (pizza) {
+  this.pizzas.push(pizza);
+  this.numPizzas++;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,72 @@
-describe('pizza', function() {
-  it("has a crust type", function() {
-    var pizza = new Pizza('thick');
+describe('Toppings', function () {
+  it('has many toppings', function () {
+    var toppings = new Toppings('sausage', 'moar sausage', 'cheese');
+    expect(toppings[0]).to.equal("sausage");
+    expect(toppings[1]).to.equal("moar sausage");
+    expect(toppings[2]).to.equal("cheese");
+  });
+
+  it('has only as many toppings as arguments', function () {
+    var toppings = new Toppings('sausage', 'moar sausage', 'cheese');
+    expect(toppings[0]).to.equal("sausage");
+    expect(toppings[1]).to.equal("moar sausage");
+    expect(toppings[2]).to.equal("cheese");
+    expect(typeof toppings[3]).to.equal("undefined");
+  });
+});
+
+describe('Object', function () {
+  describe('size', function () {
+    it('returns the length of the array of property names for a given Object', function () {
+      var toppings = new Toppings('sausage', 'moar sausage', 'cheese');
+      expect(Object.size(toppings)).to.equal(3);
+    })
+  })
+});
+
+describe('Pizza', function () {
+  it("has a size", function () {
+    var pizza = new Pizza('huge');
+    expect(pizza.crustSize).to.equal('huge');
+  });
+
+  it("has a crust type", function () {
+    var pizza = new Pizza('huge', 'thick');
     expect(pizza.crust).to.equal('thick');
   });
 
-  it("has a sauce type", function() {
-    var pizza = new Pizza('thick', 'red');
+  it("has a sauce type", function () {
+    var pizza = new Pizza('huge', 'thick', 'red');
     expect(pizza.sauce).to.equal('red');
   });
 
-  it("has toppings type", function() {
-    var toppings = new Toppings();
-    var pizza = new Pizza('thick', 'red', toppings);
+  it("has toppings", function () {
+    var toppings = new Toppings('stuff', 'things');
+    var pizza    = new Pizza('huge', 'thick', 'red', toppings);
     expect(pizza.toppings).to.equal(toppings);
   });
+});
 
+describe('Order', function () {
+  describe('save', function () {
+    it('saves a pizza to an order', function () {
+      var toppings = new Toppings('stuff', 'things');
+      var pizza    = new Pizza('huge', 'thick', 'red', toppings);
+      var pizza2   = new Pizza('small', 'thin', 'alfredo', toppings);
+      var order    = new Order();
+      order.save(pizza);
+      order.save(pizza2);
+      expect(order.pizzas).to.eql([pizza, pizza2]);
+    });
+  });
+
+  it('knows the number of pizzas in an order', function () {
+    var order     = new Order();
+    var toppings  = new Toppings('stuff', 'things');
+    var pizza     = new Pizza('huge', 'thick', 'red', toppings);
+    var pizza2    = new Pizza('small', 'thin', 'alfredo', toppings);
+    order.save(pizza);
+    order.save(pizza2);
+    expect(order.numPizzas).to.equal(2);
+  })
 });
